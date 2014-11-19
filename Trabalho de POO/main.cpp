efb1493d113eec361532e7c61952e7fba7ec70ea
Quantum2 <rafaelfrancisco6@gmail.com>
2014-11-19 12:51:51 +0000

    Prototipo de funçao de arte de menu

--- Trabalho de POO/main.cpp
+++ Trabalho de POO/main.cpp
@@ -1,9 +1,63 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+void imprimirMenu(){
+	ifstream loadFich;
+	int numRand;
+	string temp;
+
+	srand(time(NULL));
+	numRand = (rand() % 3) + 1;    //Gerar um numero aleatorio entre 1 e 3 (itens de arte do menu)
+	cout << "A carregar ficheiro de menu "<< numRand << "...\n" << endl;       //Funçda de debug - LEMBRAR DE APAGAR
+
+	switch (numRand)
+	{
+	case 1:
+		loadFich.open("men1.txt");  //Aqui começa a carregar os ficheiros com a arte de menu
+
+		if (loadFich.is_open()){
+			while(!loadFich.eof()){
+				getline(loadFich,temp);
+				cout << temp;
+			}
+		}
+		loadFich.close();
+		break;
+	case 2:
+		loadFich.open("men2.txt");
+
+		if (loadFich.is_open()){
+			while(!loadFich.eof()){
+				getline(loadFich,temp);
+				cout << temp;
+			}
+		}
+		loadFich.close();
+		break;
+	case 3:
+		loadFich.open("men3.txt");
+
+		if (loadFich.is_open()){
+			while(!loadFich.eof()){
+				getline(loadFich,temp);
+				cout << temp;
+			}
+		}
+		loadFich.close();
+		break;
+	default:
+		break;
+	}
+}
+
 int main(){
 
+	imprimirMenu();
+	
 	return 0;
 }
